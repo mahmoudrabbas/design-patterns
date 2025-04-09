485119a146c611b122a708bf7aca6e2525fd390b
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-04-09 21:03:15 +0200

    factory design pattern

---
+++ src/patterns/creational/Factory/Main.java
@@ -0,0 +1,18 @@
+package FactoryDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape circle = factory.getShape("circle");
+        circle.draw();
+
+        Shape rectangle = factory.getShape("rectangle");
+        rectangle.draw();
+
+        Shape square = factory.getShape("square");
+        square.draw();
+
+
+    }
+}
---
+++ src/patterns/creational/Factory/ShapeFactory.java
@@ -0,0 +1,19 @@
+package FactoryDesignPattern;
+
+public class ShapeFactory {
+
+    public Shape getShape(String shape){
+
+        if(shape.isBlank()) return null;
+
+        if(shape.equalsIgnoreCase("circle")){
+            return new Circle();
+        } else if (shape.equalsIgnoreCase("rectangle")) {
+            return new Rectangle();
+        }else if (shape.equalsIgnoreCase("square")) {
+            return new Square();
+        } else {
+            return null;
+        }
+    }
+}
